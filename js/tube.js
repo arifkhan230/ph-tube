343f7f32e49768d6013524def6c3111f33234db0
arifkhan101 <103602241+arifkhan101@users.noreply.github.com>
2023-09-02 15:39:13 +0600

    Done p-tube project

--- js/tube.js
+++ js/tube.js
@@ -2,15 +2,13 @@ const loadData = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const data = await response.json()
     const categories = data.data;
-    // console.log(categories)
     displayCategory(categories)
 }
 
 const displayCategory = (categories) => {
-    // console.log(categories)
     const categoryContainer = document.getElementById('category-container');
     categories.forEach((category) => {
-        // console.log(category);
+        console.log(category);
         const div = document.createElement('div');
         div.innerHTML = `
         <button onclick ="loadCategoryData('${category.category_id}')" class="bg-gray-200 text-2xl text-[#252525B3] px-5 py-2 rounded">${category.category}</button>
@@ -25,7 +23,6 @@ const displayCategory = (categories) => {
 const loadCategoryData = async (id) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const cardData = await response.json()
-    console.log(cardData)
 
     const noDataContainer = document.getElementById('no-data-container')
     noDataContainer.innerHTML = ''
@@ -42,13 +39,12 @@ const loadCategoryData = async (id) => {
         `;
         noDataContainer.appendChild(div)
     }
+    sortData(cardData)
 
-    
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML = ''
     
     cardData.data.forEach((card) => {
-        console.log(card)
 
         let publishedDate = document.createElement('div')
         publishedDate.classList.add('absolute', 'bottom-5', 'right-10','bg-black')
@@ -57,8 +53,6 @@ const loadCategoryData = async (id) => {
         if(seconds){
             const hours = parseInt ((seconds / 60) /60);
             const minute = parseInt((seconds % (60 * 60)) / 60 )
-            console.log(hours)
-            console.log(minute)
             if(minute > 0){
                 const h5 = document.createElement('h5')
                 h5.classList.add('text-white','font-medium')  
@@ -70,7 +64,7 @@ const loadCategoryData = async (id) => {
         div.innerHTML = `
         <div class="card bg-base-100">
             <figure id='published-date-container' class= "relative">
-                <img class= "h-[200px] w-[312px] rounded-lg"  src= ${card.thumbnail} />
+                <img class= "h-[200px] w-full rounded-lg"  src= ${card.thumbnail} />
                 
             </figure>
             <div class="card-body">
@@ -99,13 +93,74 @@ const loadCategoryData = async (id) => {
         publishedContainer.appendChild(publishedDate)
         cardContainer.appendChild(div);
     })
+
     
 }
 
+const sortData = (data) => {
+    document.getElementById('sort-button').addEventListener('click',()=>{
+        const sortData = data.data.sort((a,b) => parseFloat(b.others.views) - parseFloat(a.others.views))
+        const cardContainer = document.getElementById('card-container')
+    cardContainer.innerHTML = ''
+    
+    sortData.forEach((card) => {
+
+        let publishedDate = document.createElement('div')
+        publishedDate.classList.add('absolute', 'bottom-5', 'right-10','bg-black')
+
+        const seconds = card.others.posted_date
+        if(seconds){
+            const hours = parseInt ((seconds / 60) /60);
+            const minute = parseInt((seconds % (60 * 60)) / 60 )
+            if(minute > 0){
+                const h5 = document.createElement('h5')
+                h5.classList.add('text-white','font-medium')  
+                h5.innerHTML = ` ${hours}hr ${minute} min ago`;
+                publishedDate.appendChild(h5)
+            }
+        }
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card bg-base-100">
+            <figure id='published-date-container' class= "relative">
+                <img class= "h-[200px] w-full rounded-lg"  src= ${card.thumbnail} />
+                
+            </figure>
+            <div class="card-body">
+                <div class = "flex gap-3">
+                    <div>
+                        <img class="rounded-full w-[40px] h-[40px]" src=${card.authors[0].profile_picture
+                        } alt="">
+                       
+                    </div>
+                    <div>
+                        <h2 class="card-title text-2xl text-[#171717] font-bold mb-3">${card.title}</h2>
+                        <div class= "flex gap-2">
+                            <h3 class= "text-[14px] font-medium text-[#171717B3] mb-3"> ${card.authors[0].profile_name}</h3>
+                            <p>
+                                ${card.authors[0].verified? `<img class= "h-[20px] w-[20px]" src="images/tick.png" alt=""></img>` : ''} 
+                            </p>
+                        </div>
+                        <h3 class= "text-[14px] font-medium text-[#171717B3] mb-3">${card.others.views} views</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        `;
+        const publishedContainer = div.querySelector('#published-date-container');
+        publishedContainer.appendChild(publishedDate)
+        cardContainer.appendChild(div);
+    })
+
+    })
+}
+
+
 
 const handleBlog = () =>{
-    console.log('hello')
     window.location.href = "blog.html"
 }
 
+loadCategoryData(1000)
 loadData()
